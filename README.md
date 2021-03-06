1916f6ca73c11ee3349bb20b8028e32ac6eec149
James Lovejoy <james.lovejoy2@gmail.com>
2021-03-05 21:17:20 -0600

    created basic main.ts to show the use of built in generic type Array

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # typescript_generics
+
 Pluralsight refresher course on typescript generics
+
+- class github
+- bricewilson/Creating-and-Using-Generics-in-Typescript
---
+++ main.ts
@@ -0,0 +1,8 @@
+let junkDrawer: any[] = ['cool string', 42, true];
+
+//generic array type helps with typescript inference
+let companies: Array<string> = ['Microsoft', 'Google', 'Amazon'];
+
+let primeNums: Array<number> = [7, 11, 13];
+
+let lastValue = primeNums.pop();
