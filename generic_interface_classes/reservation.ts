d1e34249173de8f96a6366f5e0c62107903caa66
James Lovejoy <james.lovejoy2@gmail.com>
2021-03-08 08:30:30 -0600

    added example with type constraints of class

---
+++ generic_interface_classes/reservation.ts
@@ -0,0 +1,17 @@
+import { ConferenceRoom, conferenceRoomData } from "./models/conferenceRoom";
+import { MeetingResource } from "./models/meetingResource";
+
+class Reservation<T extends MeetingResource> {
+  reservationDate: Date;
+  organizerName: string;
+  resource: T;
+
+  requestResource(requestedResource: T, requester: string) {
+    this.resource = requestedResource;
+    this.organizerName = requester;
+    console.log(`${requester} requested a reservation ${requestedResource.name}`)
+  }
+}
+
+let teamMeeting = new Reservation<ConferenceRoom>();
+teamMeeting.requestResource(conferenceRoomData[0], 'Gary')
